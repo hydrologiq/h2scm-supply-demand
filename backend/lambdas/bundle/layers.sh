6f18e747a65b888871bb536354228b2b3a08882e
Adam Ayre-Storie <adam.ayre-storie@hydrologiq.com>
2024-01-26 11:31:15 +0000

    🎨 (bundle): Fix lambda layer structure
    make all files top level

--- backend/lambdas/bundle/layers.sh
+++ backend/lambdas/bundle/layers.sh
@@ -10,12 +10,26 @@ for d in ../layers/*/ ; do
     cd $CWD
     FILE_NAME=$(echo "$d" | sed -r 's/[./]|layers+//g')
     DIST_FILE_PATH=$DIST_DIR/$FILE_NAME
+    DIST_PYTHON_TMP=$DIST_FILE_PATH/tmp
+    DIST_PYTHON_OUT=$DIST_FILE_PATH/python
+    mkdir $DIST_FILE_PATH
+    mkdir $DIST_PYTHON_OUT
+    rm -rf .venv
+    python -m venv .venv
+    source .venv/bin/activate
     cd $d
     rm -rf dist
+    
+
     [[ -f "./pre-build.sh" ]] && . ./pre-build.sh
-    poetry build
-    poetry run pip install --upgrade -t $DIST_FILE_PATH dist/*.whl
+
+    poetry build    
+    poetry run pip install --upgrade -t $DIST_PYTHON_TMP dist/*.whl
+    cd $DIST_PYTHON_TMP
+    rsync -av --progress . /$DIST_PYTHON_OUT --exclude __pycache__ --exclude dist
     cd $DIST_FILE_PATH
+    rm -rf $DIST_PYTHON_TMP
+
     find . -exec touch -d "2023-06-01T09:00:00" {} +
     zip -rq -D -X -9 -A --compression-method deflate ../$FILE_NAME.zip . -x '*.pyc'
     touch -d "2023-06-01T09:00:00" ../$FILE_NAME.zip
